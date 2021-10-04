caf7f6f2e8d67422b5896895418e61989ef25227
coqoa <choibm0208@gmail.com>
2021-10-04 18:36:47 +0900

    211004 타이머 기본기능 구현

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//네비 날짜
 function setClock(){
     let dateObject = new Date();
     let hour = addStringZero(dateObject.getHours());
@@ -9,15 +10,143 @@ function setClock(){
     document.getElementById("POMOTODO__clock").innerHTML = year + ". " + month + ". " + date + "ﾠ " + hour+ " : " + min ; 
 
 }
-
 function addStringZero(time){
     if(parseInt(time)<10){
         return "0"+time;
     }else
         return time;
 }
-
 window.onload = function(){
     setClock();
     setInterval(setClock,1000);
 }
+//뽀모도로타이머
+let minutes = 0;
+let seconds = 0;
+
+const appendMinutes = document.getElementById("minute");
+const appendSeconds = document.getElementById("second");
+
+const buttonStart = document.getElementById("btn__start");
+const buttonStop = document.getElementById("btn__stop");
+const buttonPause = document.getElementById("btn__pause")
+
+const buttonMinute1Plus = document.getElementById("minute1__plus");
+const buttonMinute2Plus = document.getElementById("minute2__plus");
+const buttonSecond1Plus = document.getElementById("second1__plus");
+const buttonSecond2Plus = document.getElementById("second2__plus");
+const buttonMinute1Minus = document.getElementById("minute1__minus");
+const buttonMinute2Minus = document.getElementById("minute2__minus");
+const buttonSecond1Minus = document.getElementById("second1__minus");
+const buttonSecond2Minus = document.getElementById("second2__minus");
+
+let intervalID;
+
+
+buttonStart.onclick = function(){
+    clearInterval(intervalID) 
+    intervalID = setInterval(operateTimer, 1000)
+
+}
+
+buttonPause.onclick = function(){ //pause   버튼 만들기
+    clearInterval(intervalID) 
+}
+
+buttonStop.onclick = function(){ //pause   버튼 만들기
+    clearInterval(intervalID) 
+    minutes = 0; seconds = 0;
+    appendMinutes.textContent = "00"
+    appendSeconds.textContent = "00"
+}
+
+function operateTimer(){ //1초씩 감소시키기
+    // if(minutes = 0 && seconds >= 0){
+        if(minutes<10)
+            appendMinutes.textContent="0"+minutes;
+        seconds--; 
+        appendSeconds.textContent = seconds;
+        if(seconds<10)
+            appendSeconds.textContent="0"+seconds;
+        if(seconds<0){
+            minutes--;
+            appendMinutes.textContent = minutes;
+            seconds = 59;
+            appendSeconds.textContent = seconds;
+            if(minutes<10)
+            appendMinutes.textContent="0"+minutes;
+        }
+        if(minutes === 0 && seconds >= 0){
+            clearInterval(intervalID) 
+            minutes = 0; seconds = 0;
+            appendMinutes.textContent = "00"
+            appendSeconds.textContent = "00"
+        }
+    // }
+}
+//minutes증감 버튼
+buttonMinute1Plus.onclick = function(){
+    if(minutes<90){
+        minutes+=10;
+        appendMinutes.textContent=minutes;
+    }
+}
+buttonMinute2Plus.onclick = function(){
+    if(minutes<99){
+        minutes++;
+        appendMinutes.textContent=minutes;
+        if(minutes<10)
+            appendMinutes.textContent="0"+minutes
+    }
+}
+buttonMinute1Minus.onclick = function(){
+    if(minutes>9){
+        minutes-=10;
+        appendMinutes.textContent=minutes;
+        if(minutes<10)
+            appendMinutes.textContent="0"+minutes;
+    }
+}
+buttonMinute2Minus.onclick = function(){
+    if(minutes>0){
+        minutes--;
+        appendMinutes.textContent=minutes;
+        if(minutes<10)
+            appendMinutes.textContent="0"+minutes;
+    }
+}
+//
+//seconds 증감 버튼
+buttonSecond1Plus.onclick = function(){
+    if(seconds<50){
+        seconds+=10;
+        appendSeconds.textContent=seconds;
+        // if(seconds === 60)
+        //     alert('60이다')
+    }
+}
+buttonSecond2Plus.onclick = function(){
+    if(seconds<59){
+        seconds++;
+        appendSeconds.textContent=seconds;
+        if(seconds<10)
+            appendSeconds.textContent="0"+seconds;
+    }
+}
+buttonSecond1Minus.onclick = function(){
+    if(seconds>10){
+        seconds-=10;
+        appendSeconds.textContent=seconds;
+        if(seconds<10)
+            appendSeconds.textContent="0"+seconds;
+    }
+}
+buttonSecond2Minus.onclick = function(){
+    if(seconds>0){
+        seconds--;
+        appendSeconds.textContent=seconds;
+        if(seconds<10)
+                appendSeconds.textContent="0"+seconds;
+    }
+}
+//
