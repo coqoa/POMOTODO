7da7f29544dcca4ada577367a3b35e85de4647ce
coqoa <choibm0208@gmail.com>
2021-11-28 16:57:43 +0900

    211128 회원가입시 비밀번호 중복 체크

--- public/signup.js
+++ public/signup.js
@@ -2,15 +2,20 @@ function signup(){
     var signupForm = document.signupForm;
     var loginId = signupForm.loginId.value;
     var password = signupForm.password.value;
+    var passwordCheck = signupForm.passwordCheck.value;
 
     var email = signupForm.email.value;
     var number = signupForm.number.value;
     var birthday = signupForm.birthday.value;
     var gender = signupForm.gender.value;
     
-    if(!loginId || !password || !email || !number || !birthday || !gender){
-        alert("항목을 모두 입력해주세요.")
-    }else{
-        signupForm.submit();
+    if(!loginId || !password || !passwordCheck || !email || !number || !birthday || !gender){
+        alert("항목을 모두 입력해주세요.");
+    }else {
+        if(password === passwordCheck){
+            signupForm.submit();
+        }else{
+            alert('비밀번호가 일치하지 않습니다')
+        }
     }
 }
