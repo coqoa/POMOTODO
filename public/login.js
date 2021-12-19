5fff92a1570b8ed4c0796fc1799af0ea96733a85
coqoa <choibm0208@gmail.com>
2021-12-19 23:39:22 +0900

    211219 여러개의 브라우저로 접속시 navId가 전역변수로 설정되어 모두 바뀌는 문제 해결중

---
+++ public/login.js
@@ -0,0 +1,7 @@
+function printName(){
+    console.log('printName');
+    var loginForm = document.loginForm;
+    var loginId = loginForm.loginId.value;
+    var password = loginForm.loginPassword.value;
+    loginForm.submit(); 
+}
