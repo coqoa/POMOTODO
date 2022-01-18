9485ae8d3883dccd87341028a2a69dbaae666fe2
coqoa <choibm0208@gmail.com>
2022-01-18 15:31:56 +0900

    220118 크롬확장프로그램 배포완료

--- Chrome-Extension/CE-popup.js
+++ Chrome-Extension/CE-popup.js
@@ -3,6 +3,5 @@ let goPOMO = document.querySelector('.go-POMOTODO');
 goPOMO.addEventListener("click", go); 
 function go(){
     console.log('asd')
-    // window.location.href="https://pomotodo.kr";
     window.open('https://POMOTODO.kr/');
 }
